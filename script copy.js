daf6bcb83943c2b805d9e852f2483725a655e9f9
RudolphAttisso <attissorudolph@gmail.com>
2024-08-28 21:08:47 +0200

    revue de la logique du bouton play et developpement de la logique des bouton back et next; les titres changent bien à la suite mains ne sont diffusés que tous les deux titres

--- script copy.js
+++ script copy.js
@@ -1,5 +1,5 @@
 
-let tabPlaylist = [
+let tabPlaylist =[
     {
         author: "Silent Patner",
         title: "Blue Skies",
@@ -12,35 +12,35 @@ let tabPlaylist = [
         title: "Cartoon Hoedown",
         file: "Cartoon_Hoedown.mp3",
         image: "2.jpg"
-
+    
     },
     {
         author: "Jingle Punks",
         title: "Earthy Crust",
         file: "Earthy_Crust.mp3",
         image: "3.jpg"
-
+    
     },
     {
         title: "Hold On a Minute",
         author: "Silent Partner",
         file: "Hold_On_a_Minute.mp3",
         image: "4.jpg"
-
+    
     },
     {
         author: "City of Prague Philharmonic",
         title: "John Dunbar Theme",
         file: "JohnDunbarTheme.mp3",
         image: "5.jpg"
-
+    
     },
     {
         author: "Silent Partner",
         title: "Stay with You",
         file: "Stay_With_You.mp3",
         image: "6.jpg"
-
+    
     },
 
     {
@@ -48,7 +48,7 @@ let tabPlaylist = [
         title: "Symphony No. 5 (by Beethoven)",
         file: "Symphony_No_5_by_Beethoven.mp3",
         image: "7.jpg"
-
+    
     },
 
 ]
@@ -64,61 +64,49 @@ let isPlayed = false;
 //pour changer de musique
 let index = 0
 
-function playAndPause() {
-
-    if (isPlayed === false) {
-        //cas ou la musique n'est pas lancée
-        filePath = "audio/" + tabPlaylist[index].file;
-        mediaPlayer.src = filePath;
-        mediaPlayer.play();
-
-        isPlayed = true;
-        playButton.innerHTML = "&#10074;&#10074";
-
-        console.log(isPlayed);
-
-        //changement de symbole
-
-
-    } else {
-        mediaPlayer.pause();
-        isPlayed = false
-        console.log(isPlayed)
-        playButton.innerHTML = "&#9658";
-    }
-};
+function playAndPause(){
+    
+        if (isPlayed === false) {
+            //cas ou la musique n'est pas lancée
+            filePath = "audio/"+tabPlaylist[index].file;
+            mediaPlayer.src = filePath;
+            mediaPlayer.play();
+    
+            isPlayed = true;
+            playButton.innerHTML="&#10074;&#10074";
+            
+            console.log(isPlayed);
+            
+            //changement de symbole
+            
+            
+        } else {
+            mediaPlayer.pause();
+            isPlayed = false
+            console.log(isPlayed)
+            playButton.innerHTML="&#9658";
+        }
+    };
 
 playButton.addEventListener("click", playAndPause);
 
 const nextButton = document.getElementById("next");
 const backButton = document.getElementById("back");
 
+let isNext=false;
 
 
-//logique des boutons next et back
-function nextFunction() {
-    index = (index + 1) % tabPlaylist.length; // je ne comprends pas encore bien son utilité mais ça marche; à éclaircir
-    playAndPause(); 
+function nextFunction(){
+    index = (index + 1) % tabPlaylist.length; // increment index and wrap around to 0 if necessary
+    playAndPause(); // call playAndPause to play the new song
     playButton.click
     console.log(index)
     console.log(filePath)
-
-}
-function backFunction() {
-    if (index != 0) {
-        index = (index - 1) % tabPlaylist.length; 
-        playAndPause(); 
-        playButton.click // pourquoi ça ne marche pas?
-        console.log(index)
-        console.log(filePath)
-    }else{
-        playAndPause
-    }
+     
 
 }
 
 nextButton.addEventListener("click", nextFunction);
-backButton.addEventListener("click", backFunction);
 
 
 
